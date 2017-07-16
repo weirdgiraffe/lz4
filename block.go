b92ea633917983be65a19766f4e400d6cfc7ba9f
weirdgiraffe <giraffe@cyberzoo.xyz>
2017-07-16 10:03:16 +0200

    implement block

--- block.go
+++ block.go
@@ -11,50 +11,65 @@ import (
 	"fmt"
 )
 
-func Decompress(in []byte, out []byte) (int, error) {
+func DecompressBlock(in []byte, out []byte) (int, error) {
+	i, j := 0, 0
+	for i < len(in) {
+		ni, nj, err := DecompressSequence(in[i:], out, j)
+		if err != nil {
+			return -1, err
+		}
+		i += ni
+		j += nj
+	}
+	return j, nil
+}
+
+func DecompressSequence(in []byte, out []byte, offt int) (int, int, error) {
 	token := in[0]
-	i := 1 // token has taken position 0
 
-	literals := seqLen(token>>4, in[i:])
-	if len(out) < literals {
-		return 0, fmt.Errorf("output buffer is to small")
+	literals, pos := varlen(token>>4, in[1:])
+	pos += 1 // additional 1 byte for token itself
+
+	n := copy(out[offt:], in[pos:pos+literals])
+	if n != literals {
+		return -1, -1, fmt.Errorf("literals: in[] or out[] is too small")
 	}
-	i += seqLenAdditionalConsumed(literals)
+	pos += literals
 
-	i += copy(out, in[i:i+literals])
-
-	offt := int(in[i]) | int(in[i+1])<<8
-	if literals-offt < 0 {
-		return i, fmt.Errorf("offset must be inside literals")
+	if pos == len(in) {
+		// EOF reached
+		// parsing restrictions, check https://github.com/lz4/lz4/blob/dev/lib/lz4.c#L1171
+		return pos, literals, nil
 	}
-	i += 2
 
-	match := seqLen(token&0xf, in[i:]) + 4 // additional 4 bytes - minmatch
-	if len(out) < literals+match {
-		return 0, fmt.Errorf("output buffer is to small")
+	matchOfft := int(in[pos]) | int(in[pos+1])<<8
+	if (offt+literals)-matchOfft < 0 {
+		return -1, -1, fmt.Errorf("match offset must be inside out[]")
 	}
-	i += seqLenAdditionalConsumed(match - 4)
+	pos += 2
 
-	return literals + copy(out[literals:], out[literals-offt:literals-offt+match]), nil
+	match, n := varlen(token&0xf, in[pos:])
+	match += 4 // additional 4 bytes - minmatch
+	pos += n
+
+	n = copy(out[offt+literals:], out[offt+literals-matchOfft:offt+literals-matchOfft+match])
+	if n != match {
+		return -1, -1, fmt.Errorf("match: in[] or out[] is too small")
+	}
+	return pos, literals + match, nil
 }
 
-func seqLenAdditionalConsumed(l int) int {
-	if l < 15 {
-		return 0
-	}
-	return (l / 255) + 1
-}
-
-func seqLen(initialLen byte, b []byte) (l int) {
+func varlen(initialLen byte, b []byte) (l, n int) {
 	l = int(initialLen)
 	if l == 0 {
-		return 1
+		return
 	}
 	if l != 15 {
 		return
 	}
 	for i := range b {
 		l += int(b[i])
+		n++
 		if b[i] != 255 {
 			return
 		}
