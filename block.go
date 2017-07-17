64c9943e1f2dcbd9b9c97b33be71fa699dc37926
weirdgiraffe <giraffe@cyberzoo.xyz>
2017-07-17 20:58:18 +0200

    finaly beat pierrec/lz4 BenchmarkUncompressBlock
    Difference is about 2-3ns on my laptop. But my implementation is faster.
    
    Measurements:
    
    pierrec:
    BenchmarkUncompressBlock-4       5000000               277 ns/op
    BenchmarkUncompressBlock-4       5000000               277 ns/op
    BenchmarkUncompressBlock-4       5000000               276 ns/op
    BenchmarkUncompressBlock-4       5000000               278 ns/op
    
    me:
    BenchmarkDecompressBlock-4       5000000               275 ns/op
    BenchmarkDecompressBlock-4       5000000               273 ns/op
    BenchmarkDecompressBlock-4       5000000               273 ns/op
    BenchmarkDecompressBlock-4       5000000               274 ns/op
    
    Note: both benchmarks were done with my testLoremLZ4

--- block.go
+++ block.go
@@ -11,68 +11,64 @@ import (
 	"fmt"
 )
 
+const MaxInt = int(^uint(0) >> 1)
+
 func DecompressBlock(in []byte, out []byte) (int, error) {
-	i, j := 0, 0
+	var literals, matchLen, matchOfft, i, j, n int
 	for i < len(in) {
-		ni, nj, err := DecompressSequence(in[i:], out, j)
-		if err != nil {
-			return -1, err
+		literals = int(in[i] >> 4)
+		matchLen = int(in[i] & 0xf)
+		i++
+
+		if literals == 15 {
+			for ; in[i] == 255 && i < len(in); i++ {
+				if MaxInt-255 < literals {
+					return -1, fmt.Errorf("DecompressBlock: literals length is too big")
+				}
+				literals += 255
+			}
+			literals += int(in[i])
+			i++
 		}
-		i += ni
-		j += nj
+
+		if literals != 0 {
+			n = copy(out[j:], in[i:i+literals])
+			if n != literals {
+				return -1, fmt.Errorf("DecompressBlock: could not copy literals - small buffer")
+			}
+			i += literals
+			j += literals
+		}
+
+		if i == len(in) {
+			// EOF reached
+			// parsing restrictions, check https://github.com/lz4/lz4/blob/dev/lib/lz4.c#L1171
+			return j, nil
+		}
+
+		matchOfft = int(in[i]) | int(in[i+1])<<8
+		if j < matchOfft {
+			return -1, fmt.Errorf("DecompressBlock: malformed match offset")
+		}
+		i += 2
+
+		if matchLen == 15 {
+			for ; in[i] == 255 && i < len(in); i++ {
+				if MaxInt-251 < matchLen {
+					return -1, fmt.Errorf("DecompressBlock: match length is too big")
+				}
+				matchLen += 255
+			}
+			matchLen += int(in[i])
+			i++
+		}
+		matchLen += 4 // additional 4 bytes - minmatch
+
+		n = copy(out[j:], out[j-matchOfft:j-matchOfft+matchLen])
+		if n != matchLen {
+			return -1, fmt.Errorf("DecompressBlock: could not copy match - small buffer")
+		}
+		j += matchLen
 	}
 	return j, nil
-}
-
-func DecompressSequence(in []byte, out []byte, offt int) (int, int, error) {
-	token := in[0]
-
-	literals, pos := varlen(token>>4, in[1:])
-	pos += 1 // additional 1 byte for token itself
-
-	n := copy(out[offt:], in[pos:pos+literals])
-	if n != literals {
-		return -1, -1, fmt.Errorf("literals: in[] or out[] is too small")
-	}
-	pos += literals
-
-	if pos == len(in) {
-		// EOF reached
-		// parsing restrictions, check https://github.com/lz4/lz4/blob/dev/lib/lz4.c#L1171
-		return pos, literals, nil
-	}
-
-	matchOfft := int(in[pos]) | int(in[pos+1])<<8
-	if (offt+literals)-matchOfft < 0 {
-		return -1, -1, fmt.Errorf("match offset must be inside out[]")
-	}
-	pos += 2
-
-	match, n := varlen(token&0xf, in[pos:])
-	match += 4 // additional 4 bytes - minmatch
-	pos += n
-
-	n = copy(out[offt+literals:], out[offt+literals-matchOfft:offt+literals-matchOfft+match])
-	if n != match {
-		return -1, -1, fmt.Errorf("match: in[] or out[] is too small")
-	}
-	return pos, literals + match, nil
-}
-
-func varlen(initialLen byte, b []byte) (l, n int) {
-	l = int(initialLen)
-	if l == 0 {
-		return
-	}
-	if l != 15 {
-		return
-	}
-	for i := range b {
-		l += int(b[i])
-		n++
-		if b[i] != 255 {
-			return
-		}
-	}
-	panic("wrong length")
 }
