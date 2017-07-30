6e7c7777d409a6f8f200600379ccd9b71d9c3b09
weirdgiraffe <giraffe@cyberzoo.xyz>
2017-07-30 08:05:19 +0200

    just to save progress

--- frame.go
+++ frame.go
@@ -18,6 +18,129 @@ import (
 
 const lzMagic uint32 = 0x184d2204
 
+type Frame struct {
+	*FrameDesc
+	hash        *xxhash.XXHash32
+	in, out     []byte
+	bContentLen int
+}
+
+func NewFrame(r io.Reader) (f *Frame, err error) {
+	f = new(Frame)
+	f.FrameDesc, err = ReadFrameDesc(r)
+	if err != nil {
+		return nil, err
+	}
+	f.in = make([]byte, f.BlockMaxSize)
+	f.out = make([]byte, f.BlockMaxSize)
+	if f.HasContentChecksum {
+		f.hash = xxhash.New32()
+	}
+	return f, nil
+}
+
+func (f *Frame) DecompressBlock(r io.Reader) ([]byte, error) {
+	ulen, err := readUint32(r, f.out[:4])
+	if err != nil {
+		return nil, err
+	}
+
+	compressed := true
+	if (ulen & 0x80000000) != 0 {
+		ulen &= 0x7fffffff
+		compressed = false
+	}
+
+	bLen := int(ulen)
+	if bLen > len(f.in) {
+		return nil, fmt.Errorf("invalid block size")
+	}
+
+	fmt.Fprintf(os.Stderr,
+		"Read Block<Len: %d Compressed: %v BlockChecksum: %v>\n",
+		bLen, compressed, blockChecksum,
+	)
+
+	if bLen == 0 { // EndMark
+		return nil, nil
+	}
+
+	err = read(r, f.in[:bLen])
+	if err != nil {
+		return nil, err
+	}
+
+	if f.HasBlockChecksum {
+		bChecksum, err := readUint32(r, f.out[:4])
+		if err != nil {
+			return nil, err
+		}
+		must := xxhash.Checksum32(f.in[:bLen])
+		if bChecksum != must {
+			return nil, fmt.Errorf("block checksum mismatch")
+		}
+	}
+
+	if compressed {
+		f.bContentLen, err = DecompressBlock2(f.in[:bLen], f.out)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		f.bContentLen = copy(f.out, f.in[:bLen])
+	}
+
+	if f.hash != nil {
+		_, err = f.hash.Write(f.out[:f.bContentLen])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return nil
+}
+
+type Reader struct {
+	r io.Reader
+
+	pos    int
+	frame  *Frame
+	intBuf [8]byte
+}
+
+func NewReader(r io.Reader) *Reader {
+	return &Reader{r: r}
+}
+
+func (r *Reader) Read(b []byte) (n int, err error) {
+	if r.frame == nil {
+		// read lz4 frame
+		magic, err := readUint32(r.r, r.intBuf[:])
+		if err != nil {
+			return err
+		}
+		if magic != lzMagic {
+			return 4, fmt.Errorf("bad frame magic")
+		}
+		r.frame, err = NewFrame(r.r)
+		if err != nil {
+			return
+		}
+	}
+	var n int
+	for n != len(b) {
+		if r.pos <= r.frame.bContentLen {
+			err = r.frame.DecompressBlock(r.r)
+			if err != nil {
+				return
+			}
+			r.pos = 0
+		}
+		n += copy(b[n:], r.frame.out[r.pos:])
+	}
+
+}
+
 type Decompressor struct {
 	r io.Reader
 
